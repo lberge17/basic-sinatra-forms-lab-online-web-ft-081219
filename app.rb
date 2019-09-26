6deccfb942e4a2a3a2e71b58587199a6f2e89409
Laura Berge <lauraeberge@gmail.com>
2019-09-26 13:44:25 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,9 +8,29 @@ class App < Sinatra::Base
   
   post '/team' do
     @team_name = params["team_name"]
-    @
+    @small_forward = params["small_forward"]
+    @shooting_guard = params["shooting_guard"]
+    @power_forward = params["power_forward"]
+    @point_guard = params["point_guard"]
+    @coach = params["coach"]
+    @center = params["center"]
     
     erb :team
   end
   
 end
+
+center	
+"6"
+coach	
+"1"
+point_guard	
+"2"
+power_forward	
+"5"
+shooting_guard	
+"3"
+small_forward	
+"4"
+team_name	
+"Hello"
