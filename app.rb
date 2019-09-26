342705a4d372983fdfa571ee72aebfdb0be4fb14
Laura Berge <lauraeberge@gmail.com>
2019-09-26 13:49:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,13 +7,6 @@ class App < Sinatra::Base
   end
   
   post '/team' do
-    @team_name = params["team_name"]
-    @small_forward = params["small_forward"]
-    @shooting_guard = params["shooting_guard"]
-    @power_forward = params["power_forward"]
-    @point_guard = params["point_guard"]
-    @coach = params["coach"]
-    @center = params["center"]
     @team = params
     
     erb :team
